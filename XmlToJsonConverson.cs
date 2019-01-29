0faacf34e44f213f9ea61a3cb779d9636d08028b
HimanshKumarSinha <36501461+HimanshKumarSinha@users.noreply.github.com>
2019-01-29 11:53:39 +0530

    Converson Code

---
+++ XmlToJsonConverson.cs
@@ -0,0 +1,26 @@
+namespace JsonToXmlConversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string xml = @"<?xml version='1.0' standalone='no'?>
+<root>
+  <person id='1'>
+  <name>Alan</name>
+  <url>http://www.google.com</url>
+  </person>
+  <person id='2'>
+  <name>Louis</name>
+  <url>http://www.yahoo.com</url>
+  </person>
+</root>";
+            XmlDocument doc = new XmlDocument();
+            doc.LoadXml(xml);
+           string json = JsonConvert.SerializeXmlNode(doc);
+            Console.WriteLine(json);
+            Console.ReadLine();
+
+        }
+    }
+}
